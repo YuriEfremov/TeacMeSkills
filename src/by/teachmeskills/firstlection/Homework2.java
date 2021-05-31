5abd9cc18862b1385466a8631203ac787ca0a5d2
Yuri Efremov <yuefr2021@gmail.com>
2021-05-31 14:09:48 +0300

    FirstLection FirstWork

---
+++ src/by/teachmeskills/firstlection/Homework2.java
@@ -0,0 +1,17 @@
+package by.teachmeskills.firstlection;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        System.out.println("The possibility of a triangle check:");
+        int a = 6;
+        int b = 2;
+        int c = 3;
+
+        if ((a + b < c) || (a + c < b) || (c + b < a)) {
+            System.out.println("This triangle is impossible");
+        } else {
+            System.out.println("This triangle is possible");
+        }
+    }
+}
