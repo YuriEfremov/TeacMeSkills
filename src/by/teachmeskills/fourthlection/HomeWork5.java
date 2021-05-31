32e5714415daff5ece2403727df8c9e25c6fb94d
Yuri Efremov <yuefr2021@gmail.com>
2021-05-31 17:00:32 +0300

    Done HomeWork5.java

---
+++ src/by/teachmeskills/fourthlection/HomeWork5.java
@@ -0,0 +1,34 @@
+package by.teachmeskills.fourthlection;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        int n;
+
+        System.out.print("Input size  of a matrix please: ");
+        n = sc.nextInt();
+        int[][] arr1 = new int[n][n];
+        int[][] arr2 = new int[n][n];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = rnd.nextInt(51);
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr2[i][j] = arr1[j][i];
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
