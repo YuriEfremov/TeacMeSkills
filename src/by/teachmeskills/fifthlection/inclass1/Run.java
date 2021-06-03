d9d9e81c30a59190fdac22636398f1ac908edb01
Yuri Efremov <yuefr2021@gmail.com>
2021-06-03 15:48:35 +0300

    Add comments

---
+++ src/by/teachmeskills/fifthlection/inclass1/Run.java
@@ -0,0 +1,28 @@
+package by.teachmeskills.fifthlection.inclass1;
+
+public class Run {
+    public static void main(String[] args) {
+        //создаем 3 объекта (экземпляра) класса Phone.
+        // Объект создаётся из существующего класса при помощи ключевого слова new:
+        Phone samsung = new Phone();
+        Phone xiaomi = new Phone(858684, "Xiaomi", 150);
+        Phone iphone = new Phone(858585, "Iphone", 200);
+
+        samsung.setNumber(1591597);
+        samsung.setModel("GalaxyS10");
+        samsung.setWeight(150);
+
+        samsung.displayInfo();
+        xiaomi.displayInfo();
+        iphone.displayInfo();
+        System.out.println();
+
+        iphone.receiveCall("Vasya");
+        System.out.println(iphone.getNumber()); //а так можно?
+
+        iphone.receiveCall("Vasya", 889911);
+        //samsung.sendMessage(82564,82565,82566);
+
+
+    }
+}
